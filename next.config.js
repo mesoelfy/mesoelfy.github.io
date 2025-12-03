58a005644a026fb1f54f143189f3c9363be04a55
Steven Casteel <stevencasteel@protonmail.com>
2025-12-02 19:16:39 -0600

    fix: add nojekyll and clean config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   transpilePackages: ['three'],
 }
+
 module.exports = nextConfig
